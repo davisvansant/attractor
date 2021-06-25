8c51bc417bfef900b1b105b7a06963acb49b7cf7
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-06-24 17:23:32 -0700

    add utility enum, add path to utility enum, update tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod bootstrap;
 mod init;
 mod install;
 
+use tokio::process::Command;
+
 enum Suite {
     Buster,
     _Focal,
@@ -22,13 +24,26 @@ impl Suite {
 
 enum Utility {
     AptGet,
+    Debootstrap,
 }
 
 impl Utility {
     async fn name(&self) -> &str {
         match self {
             Utility::AptGet => "apt-get",
+            Utility::Debootstrap => "debootstrap",
         }
+    }
+
+    async fn path(&self) -> Result<std::ffi::OsString, std::io::Error> {
+        let utility_name = self.name().await;
+        let command = Command::new("/usr/bin/which")
+            .arg(utility_name)
+            .output()
+            .await?;
+        let string = String::from_utf8(command.stdout).expect("Failed to convert to String!");
+
+        Ok(std::ffi::OsString::from(string.trim()))
     }
 }
 
@@ -83,22 +98,30 @@ impl Attractor {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[tokio::test(flavor = "multi_thread")]
-//     async fn init() -> Result<(), std::io::Error> {
-//         let test_attractor = Attractor::init().await;
-//         assert!(test_attractor.is_ok());
-//         Ok(())
-//     }
-//
-//     #[tokio::test(flavor = "multi_thread")]
-//     async fn bootstrap() -> Result<(), std::io::Error> {
-//         let test_attractor = Attractor::init().await?;
-//         let test_attractor_bootstrap = test_attractor.bootstrap().await;
-//         assert!(test_attractor_bootstrap.is_ok());
-//         Ok(())
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn utility_path() -> Result<(), std::io::Error> {
+        let test_utility = Utility::AptGet;
+        let test_utility_path = test_utility.path().await?;
+        assert_eq!(test_utility_path, "/usr/bin/apt-get");
+        Ok(())
+    }
+
+    // #[tokio::test(flavor = "multi_thread")]
+    // async fn init() -> Result<(), std::io::Error> {
+    //     let test_attractor = Attractor::init().await;
+    //     assert!(test_attractor.is_ok());
+    //     Ok(())
+    // }
+    //
+    // #[tokio::test(flavor = "multi_thread")]
+    // async fn bootstrap() -> Result<(), std::io::Error> {
+    //     let test_attractor = Attractor::init().await?;
+    //     let test_attractor_bootstrap = test_attractor.bootstrap().await;
+    //     assert!(test_attractor_bootstrap.is_ok());
+    //     Ok(())
+    // }
+}
