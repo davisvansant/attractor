770c89194b8578d4e1fd08fb57a62f3de5f2e270
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-06-26 11:32:15 -0700

    initial print_debs

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -2,6 +2,22 @@ use crate::{Attractor, Suite, Utility, Variant};
 
 use tokio::process::Command;
 
+async fn print_debs(variant: Variant, suite: Suite) -> Result<bool, std::io::Error> {
+    let name = variant.name().await;
+    let code_name = suite.code_name().await;
+    let target = "/var/opt/attractor/tmp".to_owned() + name + code_name;
+    let command_path = Utility::Debootstrap.path().await?;
+    let command = Command::new(&command_path)
+        .current_dir("/var/opt/attractor")
+        .arg("--print-debs")
+        .arg(code_name)
+        .arg(target)
+        .status()
+        .await?;
+
+    Ok(command.success())
+}
+
 async fn make_tarball(variant: Variant, suite: Suite) -> Result<bool, std::io::Error> {
     let name = variant.name().await;
     let kind = variant.kind().await;
@@ -23,6 +39,8 @@ async fn make_tarball(variant: Variant, suite: Suite) -> Result<bool, std::io::E
 
 impl Attractor {
     pub(crate) async fn run_bootrap() -> Result<(), std::io::Error> {
+        print_debs(Variant::Minbase, Suite::Buster).await?;
+        print_debs(Variant::Buildd, Suite::Buster).await?;
         make_tarball(Variant::Minbase, Suite::Buster).await?;
         make_tarball(Variant::Buildd, Suite::Buster).await?;
         Ok(())
