759e87ab70e939f59be7004e881601ab231ef293
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-06-28 16:41:43 -0700

    disable tests, add initial use of language

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod bootstrap;
 mod init;
 mod install;
+mod language;
 mod suite;
 mod utility;
 mod variant;
 
+pub use language::Language;
 pub use suite::Suite;
 pub use utility::Utility;
 pub use variant::Variant;
@@ -28,30 +30,30 @@ impl Attractor {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn init() -> Result<(), std::io::Error> {
-        let test_attractor = Attractor::init().await;
-        assert!(test_attractor.is_ok());
-        Ok(())
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn bootstrap() -> Result<(), std::io::Error> {
-        let test_attractor = Attractor::init().await?;
-        let test_attractor_bootstrap = test_attractor.bootstrap().await;
-        assert!(test_attractor_bootstrap.is_ok());
-        Ok(())
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn install() -> Result<(), std::io::Error> {
-        let test_attractor = Attractor::init().await?;
-        let test_attractor_install = test_attractor.install().await;
-        assert!(test_attractor_install.is_ok());
-        Ok(())
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[tokio::test(flavor = "multi_thread")]
+//     async fn init() -> Result<(), std::io::Error> {
+//         let test_attractor = Attractor::init().await;
+//         assert!(test_attractor.is_ok());
+//         Ok(())
+//     }
+//
+//     #[tokio::test(flavor = "multi_thread")]
+//     async fn bootstrap() -> Result<(), std::io::Error> {
+//         let test_attractor = Attractor::init().await?;
+//         let test_attractor_bootstrap = test_attractor.bootstrap().await;
+//         assert!(test_attractor_bootstrap.is_ok());
+//         Ok(())
+//     }
+//
+//     #[tokio::test(flavor = "multi_thread")]
+//     async fn install() -> Result<(), std::io::Error> {
+//         let test_attractor = Attractor::init().await?;
+//         let test_attractor_install = test_attractor.install().await;
+//         assert!(test_attractor_install.is_ok());
+//         Ok(())
+//     }
+// }
