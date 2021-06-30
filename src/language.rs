83b8125f6e16f9c0dee9b6e1606304cb9ba24e88
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-06-30 11:06:37 -0700

    initial download, tests

--- src/language.rs
+++ src/language.rs
@@ -1,3 +1,7 @@
+use crate::Utility;
+
+use tokio::process::Command;
+
 pub enum Language {
     Rust,
 }
@@ -14,6 +18,19 @@ impl Language {
                 _ => panic!("Version unsupported!"),
             },
         }
+    }
+
+    pub async fn download(&self, release: &str) -> Result<bool, std::io::Error> {
+        let tmp_path = "/var/opt/attractor/tmp";
+        let wget = Utility::Wget.path().await?;
+
+        let download = Command::new(&wget)
+            .arg(release)
+            .current_dir(tmp_path)
+            .status()
+            .await?;
+
+        Ok(download.success())
     }
 }
 
@@ -37,5 +54,17 @@ mod tests {
     async fn language_rust_incorrect_version() {
         let test_language = Language::Rust;
         test_language.release("1.49.0").await;
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn language_rust_download() -> Result<(), std::io::Error> {
+        let test_language = Language::Rust;
+        let test_release = test_language.release("1.53.0").await;
+        let test_language_download = test_language.download(test_release).await?;
+        assert!(test_language_download);
+        let test_download = "/var/opt/attractor/tmp/rust-1.53.0-x86_64-unknown-linux-gnu.tar.gz";
+        let test_metadata_target = tokio::fs::metadata(test_download).await?;
+        assert!(test_metadata_target.is_file());
+        Ok(())
     }
 }
