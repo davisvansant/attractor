61ee2106b9af5bdd19862f152a1217ba874e43af
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-01 11:49:18 -0700

    add new field, update build, tests

--- src/chroot.rs
+++ src/chroot.rs
@@ -1,8 +1,18 @@
-pub struct Chroot {}
+use crate::Suite;
+
+use std::path::PathBuf;
+use std::str::FromStr;
+
+pub struct Chroot {
+    pub directory: PathBuf,
+}
 
 impl Chroot {
-    pub async fn build() -> Result<Chroot, std::io::Error> {
-        Ok(Chroot {})
+    pub async fn build(kind: Suite) -> Result<Chroot, Box<dyn std::error::Error>> {
+        let path = kind.buildd_path().await;
+        let directory = PathBuf::from_str(path)?;
+
+        Ok(Chroot { directory })
     }
 }
 
@@ -11,9 +21,50 @@ mod tests {
     use super::*;
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn build() -> Result<(), std::io::Error> {
-        let test_chroot = Chroot::build().await;
+    async fn build_buster() -> Result<(), std::io::Error> {
+        let test_suite = Suite::Buster;
+        let test_chroot = Chroot::build(test_suite).await;
         assert!(test_chroot.is_ok());
+        assert_eq!(
+            test_chroot.unwrap().directory.to_str().unwrap(),
+            "/var/opt/attractor/buildd/buster",
+        );
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn build_focal() -> Result<(), std::io::Error> {
+        let test_suite = Suite::_Focal;
+        let test_chroot = Chroot::build(test_suite).await;
+        assert!(test_chroot.is_ok());
+        assert_eq!(
+            test_chroot.unwrap().directory.to_str().unwrap(),
+            "/var/opt/attractor/buildd/focal",
+        );
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn build_bionic() -> Result<(), std::io::Error> {
+        let test_suite = Suite::_Bionic;
+        let test_chroot = Chroot::build(test_suite).await;
+        assert!(test_chroot.is_ok());
+        assert_eq!(
+            test_chroot.unwrap().directory.to_str().unwrap(),
+            "/var/opt/attractor/buildd/bionic",
+        );
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn build_xenial() -> Result<(), std::io::Error> {
+        let test_suite = Suite::_Xenial;
+        let test_chroot = Chroot::build(test_suite).await;
+        assert!(test_chroot.is_ok());
+        assert_eq!(
+            test_chroot.unwrap().directory.to_str().unwrap(),
+            "/var/opt/attractor/buildd/xenial",
+        );
         Ok(())
     }
 }
